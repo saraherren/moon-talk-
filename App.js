4b402a861127370064a204dc47d4830801eb5cfa
sara herren <dearestfairyantiques@gmail.com>
2026-03-14 21:53:41 -0400

    Set up React Navigation with TabNavigator

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigator from "./navigation/TabNavigator';
 
+export default function App() {
+    retturn (
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    );
+}
